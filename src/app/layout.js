1f429b2eaf51b9d70b5096dae317b3d0e3c16180
vrude-1 <147126435+vrude-1@users.noreply.github.com>
2025-03-06 23:49:27 +0530

    Reinstall dependencies

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Header from "../components/Header";
+import Header from "../../components/Header";
 import { ClerkProvider } from "@clerk/nextjs";
 
 const geistSans = Geist({
